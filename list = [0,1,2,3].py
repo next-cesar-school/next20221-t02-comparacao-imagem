5c76f9556479892ceef69f8dbbb829238e7e68b5
Bruna Veiga <104539781+brunaveigacampos@users.noreply.github.com>
2022-06-13 19:47:06 -0300

    Delete list = [0,1,2,3].py

--- list = [0,1,2,3].py
+++
@@ -1,7 +0,0 @@
-list = [0,1,2,3] 
-dir_list_final = []
-id_arq = []    
-for i in range(len(list)):
-    id_arq.append(i+1)
-    dir_list_final.append(f"{id_arq[i]} - {list[i]}")
-print(dir_list_final)
